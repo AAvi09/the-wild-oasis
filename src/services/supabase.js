4650cd8b98b41612cb45fc78eccef633649d4aa4
Avinash sharma <sharmaavi2196@gmail.com>
2025-05-14 19:32:20 +0530

    feat: add Supabase client and implement getCabins function for data retrieval

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://yeyzaxhytltmthzyfmth.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlleXpheGh5dGx0bXRoenlmbXRoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY0NTk4NjMsImV4cCI6MjA2MjAzNTg2M30.Fc75biKD9dFhDXZsiqt7_pwJE7vB5QLe__pI3w2cRwA";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
